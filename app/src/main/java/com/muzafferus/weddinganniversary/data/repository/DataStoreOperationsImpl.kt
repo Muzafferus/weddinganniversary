4da6d2bbcaeebdd8bd76b57b8a1b963233be4567
muzeffer.pasazade <muzeffer.pasazade@1tech.co>
2023-02-14 00:15:25 +0400

    Home Screen data empty list and simple data show for array

--- app/src/main/java/com/muzafferus/weddinganniversary/data/repository/DataStoreOperationsImpl.kt
+++ app/src/main/java/com/muzafferus/weddinganniversary/data/repository/DataStoreOperationsImpl.kt
@@ -11,7 +11,13 @@ class DataStoreOperationsImpl(context: Context) : DataStoreOperations {
     override fun getCoupleDataState(): Flow<ArrayList<CoupleData>> {
         val flowValue: Flow<ArrayList<CoupleData>>
         return flow {
-            emit(arrayListOf(CoupleData("CoupleName", "CoupleId", "123456789")))
+            emit(arrayListOf(
+                CoupleData("OldCouple", "123", "123456789"),
+                CoupleData("NewCouple", "654", "15648979"),
+                CoupleData("BestCouple", "789", "12156156"),
+                CoupleData("LastCouple", "1594", "11194815"),
+                CoupleData("FirstCouple", "9547", "1598454"),
+            ))
         }
     }
 }
--- app/src/main/java/com/muzafferus/weddinganniversary/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/muzafferus/weddinganniversary/presentation/screens/home/HomeScreen.kt
@@ -1,7 +1,127 @@
 package com.muzafferus.weddinganniversary.presentation.screens.home
 
+import android.content.res.Configuration
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.muzafferus.weddinganniversary.R
+import com.muzafferus.weddinganniversary.domain.model.CoupleData
+import com.muzafferus.weddinganniversary.ui.theme.DarkGray
+import com.muzafferus.weddinganniversary.ui.theme.LightGray
+import com.muzafferus.weddinganniversary.ui.theme.NETWORK_ERROR_ICON_HEIGHT
+import com.muzafferus.weddinganniversary.ui.theme.SMALL_PADDING
 
 @Composable
-fun HomeScreen (navController: NavHostController){}
+fun HomeScreen(
+    navController: NavHostController,
+    homeViewModel: HomeViewModel = hiltViewModel()
+) {
+    val coupleDataCompleted by homeViewModel.coupleData.collectAsState()
+
+    if (coupleDataCompleted.isEmpty()) {
+        EmptyList()
+    } else {
+        ListContent(coupleDataCompleted)
+    }
+}
+
+@Composable
+fun EmptyList() {
+    EmptyContent(
+        alphaAnim = ContentAlpha.disabled,
+        icon = R.drawable.ic_network_error,
+        message = "List is empty"
+    )
+}
+
+@Composable
+fun EmptyContent(
+    alphaAnim: Float,
+    icon: Int,
+    message: String
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            modifier = Modifier
+                .size(NETWORK_ERROR_ICON_HEIGHT)
+                .alpha(alpha = alphaAnim),
+            painter = painterResource(id = icon),
+            contentDescription = stringResource(id = R.string.network_error_icon),
+            tint = if (isSystemInDarkTheme()) LightGray else DarkGray
+        )
+        Text(
+            modifier = Modifier
+                .padding(top = SMALL_PADDING)
+                .alpha(alpha = alphaAnim),
+            text = message,
+            color = if (isSystemInDarkTheme()) LightGray else DarkGray,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Medium,
+            fontSize = MaterialTheme.typography.subtitle1.fontSize
+        )
+
+    }
+}
+
+
+@Composable
+fun ListContent(data: ArrayList<CoupleData>) {
+    LazyColumn(
+        contentPadding = PaddingValues(all = SMALL_PADDING),
+        verticalArrangement = Arrangement.spacedBy(SMALL_PADDING)
+    ) {
+        items(data) { couple ->
+            CoupleItem(couple)
+        }
+    }
+}
+
+@Composable
+fun CoupleItem(data: CoupleData) {
+    Text(text = data.coupleNames)
+    Text(text = data.coupleId)
+    Text(text = data.coupleWeddingDate)
+}
+
+@Composable
+@Preview(showBackground = true)
+fun EmptyScreenPreview() {
+    EmptyContent(
+        alphaAnim = ContentAlpha.disabled,
+        icon = R.drawable.ic_network_error,
+        message = "Internet Unavailable."
+    )
+}
+
+@Composable
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+fun EmptyScreenDarkPreview() {
+    EmptyContent(
+        alphaAnim = ContentAlpha.disabled,
+        icon = R.drawable.ic_network_error,
+        message = "Internet Unavailable."
+    )
+}
